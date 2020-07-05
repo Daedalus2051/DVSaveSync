f3f1ddb8f9c8eb29ded6fd2bcd8728d1944d47ad
daedalus2051 <SixStringTrebleTerror@gmail.com>
2020-07-05 17:53:34 -0400

    Init commit (wip)
    Initialized at published version 1.0.0.7

---
+++ DVSaveSync/Configuration.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DVSaveSync
+{
+    [Serializable]
+    public class Configuration
+    {
+        /// <summary>
+        /// Where the DV save game data is located.
+        /// </summary>
+        [JsonProperty("SaveLocation")]
+        public string SaveLocation { get; set; }
+
+        /// <summary>
+        /// Where the saves will be "uploaded" to, for now this will be limited to local/network storage.
+        /// </summary>
+        [JsonProperty("UploadLocation")]
+        public string UploadLocation { get; set; }
+
+        /// <summary>
+        /// Preference for making a backup of the savegame files.
+        /// </summary>
+        [JsonProperty("BackupOption")]
+        public BackupPreference BackupOption { get; set; }
+
+        /// <summary>
+        /// The last time the save files were updated, to ensure overwriting does not occur.
+        /// </summary>
+        [JsonProperty("LastUpdated")]
+        public DateTime LastUpdated { get; set; }
+
+        /// <summary>
+        /// Determines if the additional "_backup..." savegame files should be copied as well.
+        /// </summary>
+        [JsonProperty("IncludeBackupSaveFiles")]
+        public bool IncludeBackupSaveFiles { get; set; }
+
+        /// <summary>
+        /// Allow the program to copy a newer savegame file from the upload location to the local (overwriting the local savegame file).
+        /// </summary>
+        [JsonProperty("AllowDownloadSavegame")]
+        public bool AllowDownloadSavegame { get; set; }
+
+        /// <summary>
+        /// Keep the program running until the user presses Enter.
+        /// </summary>
+        [JsonProperty("KeepAlive")]
+        public bool KeepAlive { get; set; }
+
+        public bool BackupSaveFile()
+        {
+            bool result = false;
+            try
+            {
+                File.Copy($"{SaveLocation}\\savegame", $"{SaveLocation}\\savegame-dvss_{DateTime.Now:yyyy-dd-M-HH-mm-ss}.bak");
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Configuration.BackupSaveFile]::Error trying to backup the file!{Environment.NewLine}{ex.Message}");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Loads a file containing valid json representing the Configuration object.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns>Configuration object loaded from the json data, if data cannot be read returns new Configuration object.</returns>
+        public static Configuration LoadConfiguration(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath)) throw new ArgumentNullException("filePath");
+            if (!File.Exists(filePath)) throw new FileNotFoundException($"Error: could not find file '{filePath}'");
+
+            Configuration config = new Configuration();
+            try
+            {
+                config = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText(filePath));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Configuration.LoadConfiguration]::Error trying to open configuration file!{Environment.NewLine}{ex.Message}");
+            }
+
+            return config;
+        }
+
+        /// <summary>
+        /// If no config file exists, this method can be used 
+        /// </summary>
+        public static void GenerateDefaultConfiguration(string path)
+        {
+            Configuration config = new Configuration
+            {
+                //C:\Program Files (x86)\Steam\steamapps\common\Derail Valley
+                //C:\Program Files (x86)\Steam\steamapps\common\Derail Valley\DerailValley_Data\SaveGameData
+                SaveLocation = @"C:\Program Files (x86)\Steam\steamapps\common\Derail Valley\DerailValley_Data\SaveGameData",
+                UploadLocation = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\DVSaveSync",
+
+                BackupOption = BackupPreference.OnlyInDanger,
+                LastUpdated = new DateTime(2020, 5, 31, 21, 17, 34),
+                IncludeBackupSaveFiles = false,
+                AllowDownloadSavegame = true,
+                KeepAlive = true
+            };
+
+            string configJson = JsonConvert.SerializeObject(config);
+            File.WriteAllText($"{path}\\config.json", configJson);
+        }
+    }
+
+    public enum BackupPreference
+    {
+        DoNotBackup = 0,
+        AskForBackup = 1,
+        OnlyInDanger = 2,
+        AlwaysBackup = 3
+    }
+}
---
+++ DVSaveSync/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace DVSaveSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Console.WriteLine($"Starting DVSaveSwapper - {version}");
+
+            string dvSaveSyncDocs = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\DVSaveSync";
+            // Look for mydocs DVSaveSync folder
+            if (!Directory.Exists(dvSaveSyncDocs))
+            {
+                Console.WriteLine($"App folder doesn't exist... creating...");
+                try
+                {
+                    Directory.CreateDirectory(dvSaveSyncDocs);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to create app folder!{Environment.NewLine}{ex.Message}");
+                    Environment.Exit((int)ErrorCodes.ErrorDuringOperations);
+                }
+                Console.Write("Done.");
+            }
+
+            Console.WriteLine($"Current directory: {dvSaveSyncDocs}");
+            Configuration config = new Configuration();
+
+            if (!File.Exists($"{dvSaveSyncDocs}\\config.json"))
+            {
+                Console.WriteLine("No config information found, using defaults...");
+                Configuration.GenerateDefaultConfiguration(dvSaveSyncDocs);
+            }
+            Console.WriteLine("Loading configuration...");
+            config = Configuration.LoadConfiguration($"{dvSaveSyncDocs}\\config.json");
+
+            // Look for savegame file
+            Console.Write("Inspecting game folder for save files...");
+
+            DirectoryInfo dvDirectory = new DirectoryInfo(config.SaveLocation);
+            if (!dvDirectory.Exists)
+            {
+                Console.WriteLine($"Error, could not find savegame directory: '{config.SaveLocation}'");
+                Environment.Exit((int)ErrorCodes.CouldNotFindPath);
+            }
+
+            DirectoryInfo uploadDir = new DirectoryInfo(config.UploadLocation);
+            if (!uploadDir.Exists)
+            {
+                Console.WriteLine($"Upload location does not exist... create? Y/N");
+                string response = Console.ReadLine();
+                if (response.ToUpper() == "Y")
+                {
+                    uploadDir.Create();
+                }
+                else
+                {
+                    Console.WriteLine($"Exiting due to upload location not existing.");
+                    Environment.Exit((int)ErrorCodes.CouldNotFindPath);
+                }
+            }
+
+            FileInfo[] saveGames = dvDirectory.GetFiles("savegame*");
+            string localSavegame = "";
+
+            if (saveGames.Length > 0)
+            {
+                localSavegame = saveGames.FirstOrDefault(s => s.Name == "savegame").FullName;
+                if (string.IsNullOrEmpty(localSavegame))
+                {
+                    Console.WriteLine("No savegame files found! Is the save path correct?");
+                    Environment.Exit((int)ErrorCodes.CouldNotFindSaveFiles);
+                }
+                else
+                {
+                    Console.WriteLine("Found savegames!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No savegame files found! Is the save path correct?");
+                Environment.Exit((int)ErrorCodes.CouldNotFindSaveFiles);
+            }
+
+            FileInfo localFileinfo = new FileInfo(localSavegame);
+            // Check backup options
+            switch (config.BackupOption)
+            {
+                case BackupPreference.AlwaysBackup:
+                    // if always, make a backup of the local savegame file
+                    if (config.BackupSaveFile() != true)
+                    {
+                        Console.WriteLine("There was an error, exiting!");
+                        Environment.Exit((int)ErrorCodes.ErrorDuringOperations);
+                    }
+                    break;
+                case BackupPreference.AskForBackup:
+                    Console.WriteLine("Would you like to backup the current savegame? Y/N");
+                    string usrResponse = Console.ReadLine();
+                    if (usrResponse.ToUpper() == "Y")
+                    {
+                        // Make a backup copy of the file
+                        if (config.BackupSaveFile() != true)
+                        {
+                            Console.WriteLine("There was an error, exiting!");
+                            Environment.Exit((int)ErrorCodes.ErrorDuringOperations);
+                        }
+                    }
+
+                    break;
+                case BackupPreference.DoNotBackup:
+                case BackupPreference.OnlyInDanger:
+                    Console.WriteLine("Redundant backups not desired, moving on...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid backup option... skipping...");
+                    break;
+            }
+
+            // Check if there is a savegame file in the upload location
+            string uploadSavegame = $"{config.UploadLocation}\\savegame";
+            FileInfo uploadFileinfo = new FileInfo(uploadSavegame);
+
+            if (!File.Exists(uploadSavegame))
+            {
+                Console.WriteLine("No upload save found, copying local savegame to upload destination...");
+                DisplayFileInfos(uploadFileinfo, localFileinfo);
+                File.Copy(localSavegame, uploadSavegame);
+            }
+            else
+            {// upload save exists, check the date...is it older than what we have?
+                DateTime uploadSaveDate = new FileInfo(uploadSavegame).LastWriteTime;
+                DateTime localSaveDate = new FileInfo(localSavegame).LastWriteTime;
+
+#if DEBUG
+                Console.WriteLine($"[DEBUG]: localSaveDate-{localSaveDate.ToShortDateString()} {localSaveDate.ToShortTimeString()}");
+                Console.WriteLine($"[DEBUG]: uploadSaveDate-{uploadSaveDate.ToShortDateString()} {uploadSaveDate.ToShortTimeString()}");
+#endif
+
+                //https://docs.microsoft.com/en-us/dotnet/api/system.datetime.compare?view=netcore-3.1
+                int result = -1;
+                result = DateTime.Compare(localSaveDate, uploadSaveDate);
+                if (result < 0)
+                {// relationship is earlier than
+                    // local save is earlier than upload save = local save is older
+                    Console.WriteLine("Local save is older, copying would overwrite newer savegame...");
+                    if (config.AllowDownloadSavegame)
+                    {
+                        if (config.BackupOption == BackupPreference.OnlyInDanger)
+                        {
+                            Console.WriteLine("Backing up local savegame, just in case... (this is because of your preferences)");
+                            config.BackupSaveFile();
+                        }
+                        Console.WriteLine("Copying newer savegame from upload location...");
+                        DisplayFileInfos(uploadFileinfo, localFileinfo);
+                        File.Copy(uploadSavegame, localSavegame, true);
+                        
+                        Console.WriteLine("Savegame has been updated from [newer] Upload savegame successfully!");
+                    }
+                }
+                else if (result == 0)
+                { // relationship is the same time as
+                    Console.WriteLine("Local and upload savegame files are the same! Nothing to do here.");
+                }
+                else
+                {// relationship is later than
+                    // local save is later than upload save = local save is newer
+                    Console.WriteLine("Local savegame is newer than upload savegame. Copying local save to Upload location...");
+                    DisplayFileInfos(uploadFileinfo, localFileinfo);
+                    File.Copy(localSavegame, uploadSavegame, true);
+                    Console.WriteLine("Savegame has been copied to Upload location successfully!");
+                }
+            }
+
+            Console.Write("DVSaveSwapper has completed.");
+            if (config.KeepAlive)
+            {
+                Console.WriteLine(" Press Enter to close.");
+                Console.ReadLine();
+            }
+            Environment.Exit(0);
+        }
+
+        public enum ErrorCodes
+        {
+            NoErrors = 0,
+            CouldNotFindSaveFiles = 1,
+            CouldNotFindPath = 2,
+            ErrorDuringOperations = 3
+        }
+        public static void DisplayFileInfos(FileInfo upload, FileInfo local)
+        {
+            // TODO: figure out why the FileInfo.Length property is throwing an exception (something to do with no file extension?)
+            Console.WriteLine($"Upload file details: {upload.Name} - {upload.LastWriteTime}");
+            Console.WriteLine($"Local file details: {local.Name} - {local.LastWriteTime}");
+        }
+    }
+}
